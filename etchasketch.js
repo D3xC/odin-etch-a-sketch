19e5a04449052fc368e49c041070cb6d51804114
Dex Campbell <dextercampbell@dextercampbell.com>
2022-10-11 12:51:22 -0400

    Setup sketch pad

---
+++ etchasketch.js
@@ -0,0 +1,33 @@
+const container = document.querySelector(".container");
+
+function setLayout() {
+  const title = document.createElement("div");
+  createSketchPad(16);
+}
+
+function createSketchPad(dimension) {
+  const sketchPad = document.createElement("div");
+  sketchPad.style.width = dimension * 25;
+  sketchPad.style.height = dimension * 25;
+  sketchPad.classList.add("pad");
+  for (let i = 1; i <= dimension; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 1; j <= dimension; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      row.appendChild(cell);
+    }
+    sketchPad.appendChild(row);
+  }
+  container.appendChild(sketchPad);
+}
+
+function destroySketchPad() { }
+
+function colorCell(e) {
+  e.target.style.backgroundColor = black;
+}
+
+
+setLayout();
